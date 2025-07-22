e179c155637203beda8e445800ac2dcea4cf5169
KodawonKK <rhekdnjs0169@gmail.com>
2025-07-23 00:02:06 +0900

    fix : 등급 추가

---
+++ src/hooks/useMovieCertification.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../utils/api';
+
+const fetchMovieCertification = movieId => {
+  return api.get(`/movie/${movieId}/release_dates`);
+};
+
+export const useMoviesCertificationQuery = movieId => {
+  return useQuery({
+    queryKey: ['movie-certification', movieId],
+    queryFn: () => fetchMovieCertification(movieId),
+    enabled: !!movieId,
+    select: result => result.data.results,
+  });
+};
--- src/pages/HomePage/components/MovieCard/PreviewModal.jsx
+++ src/pages/HomePage/components/MovieCard/PreviewModal.jsx
@@ -3,22 +3,33 @@ import { createPortal } from 'react-dom';
 import './PreviewModal.style.css';
 import { useMoviesGenreQuery } from '../../../../hooks/useMoviesGenre';
 import { useMoviesDetailQuery } from '../../../../hooks/useMovieDetail';
+import { useMoviesCertificationQuery } from '../../../../hooks/useMovieCertification';
 import Rating12 from '../../../../assets/icon/rating12.svg';
 import Rating15 from '../../../../assets/icon/rating15.svg';
 import Rating19 from '../../../../assets/icon/rating19.svg';
+import RatingALL from '../../../../assets/icon/ratingAll.svg';
 
 const PreviewModal = ({ movie, position, onMouseEnter, onMouseLeave }) => {
   const { data: genreList } = useMoviesGenreQuery();
   const { data: movieInfo } = useMoviesDetailQuery(movie?.id);
+  const { data: movieGrade } = useMoviesCertificationQuery(movie?.id);
 
   const orgRuntime = movieInfo?.runtime;
   const runHours = Math.floor(orgRuntime / 60);
   const runMinutes = orgRuntime % 60;
   const runtimeKR = `${runHours}시간 ${runMinutes}분`;
+  const krGradeInfo = movieGrade?.find(item => item.iso_3166_1 === 'KR');
+  const cert = krGradeInfo?.release_dates[0]?.certification;
+  const movieCert = cert ? cert : '19';
+
+  const ratingIcons = {
+    12: Rating12,
+    15: Rating15,
+    19: Rating19,
+    ALL: RatingALL,
+  };
+
   // const releaseDate = movieInfo?.release_date;
-
-  const portal = document.getElementById('portal-root');
-
   const genreMap = useMemo(() => {
     const map = {};
     genreList?.forEach(genre => {
@@ -27,6 +38,7 @@ const PreviewModal = ({ movie, position, onMouseEnter, onMouseLeave }) => {
     return map;
   }, [genreList]);
 
+  const portal = document.getElementById('portal-root');
   if (!portal || !movie) return null;
 
   const style = {
@@ -48,7 +60,7 @@ const PreviewModal = ({ movie, position, onMouseEnter, onMouseLeave }) => {
       />
       <div className="preview-movie-info">
         <div className="movie-info">
-          <img src={Rating12} alt="등급" height="32px" />
+          <img src={ratingIcons[movieCert]} alt="등급" height="32px" />
           <p className="movie-runtime">{runtimeKR}</p>
         </div>
         <h4 className="movie-title">{movie?.title}</h4>
