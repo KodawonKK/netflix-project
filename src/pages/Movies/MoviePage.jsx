c680137ef399b828c7d750d16ea12c4c74fbbc02
KodawonKK <rhekdnjs0169@gmail.com>
2025-07-30 00:39:58 +0900

    feat: 영화 페이지 추가

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React from 'react';
+import ContentSlider from '../../common/ContentSlider/ContentSlider';
+import { usePopularMoviesQuery } from '../../hooks/movie/usePopularMovies';
+import { useTopRatedMoviesQuery } from '../../hooks/movie/useTopRatedMovies';
+import { useNowPlayingMoviesQuery } from '../../hooks/movie/useNowPlayingMovies';
+import { useUpcomingMoviesQuery } from '../../hooks/movie/useUpcomingMovies';
+import Banner from '../HomePage/components/Banner/Banner';
 
 const MoviePage = () => {
-  return <div>MoviePage</div>;
+  const popular = usePopularMoviesQuery();
+  const topRated = useTopRatedMoviesQuery();
+  const playing = useNowPlayingMoviesQuery();
+  const upComing = useUpcomingMoviesQuery();
+
+  const slideConfigs = [
+    {
+      title: '인기 영화',
+      data: popular.data,
+      isTopRank: false,
+    },
+    {
+      title: '오늘 TOP10 영화',
+      data: topRated.data,
+      isTopRank: true,
+    },
+    {
+      title: '현재 상영 중인 영화',
+      data: playing.data,
+      isTopRank: false,
+    },
+    {
+      title: '상영 예정 영화',
+      data: upComing.data,
+      isTopRank: false,
+    },
+  ];
+  return (
+    <div>
+      <Banner data={popular.data} kind="movie" />
+      {slideConfigs.map((config, idx) => (
+        <ContentSlider
+          key={idx}
+          title={config.title}
+          data={config.data}
+          isTopRank={config.isTopRank}
+          kind="movie"
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MoviePage;
