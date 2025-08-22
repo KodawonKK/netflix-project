a35b3d9c9d26bf60f5a872b3782324e80c48168e
KodawonKK <rhekdnjs0169@gmail.com>
2025-08-23 00:17:09 +0900

    fix : mapInfo selectedInfo 삭제

--- src/utils/mapInfo.js
+++ src/utils/mapInfo.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { formatRuntime } from './formatRuntime';
 
-export const mapInfo = (kind, selectedInfo, fullInfo) => {
+export const mapInfo = (kind, fullInfo) => {
   return {
-    imgUrl: `https://image.tmdb.org/t/p/original${selectedInfo?.backdrop_path}`,
+    imgUrl: `https://image.tmdb.org/t/p/original${fullInfo?.backdrop_path}`,
     release:
       kind === 'movie'
-        ? selectedInfo.release_date.split('-')[0]
-        : selectedInfo.seasons[0].air_date.split('-')[0],
+        ? fullInfo?.release_date.split('-')[0]
+        : fullInfo?.seasons[0]?.air_date.split('-')[0],
     runtimeKR:
       kind === 'movie'
-        ? formatRuntime(selectedInfo?.runtime)
-        : selectedInfo?.number_of_seasons === 1
-        ? selectedInfo.seasons?.[0]?.name
-        : `시즌 ${selectedInfo?.number_of_seasons}개`,
-    overView: selectedInfo?.overview,
+        ? formatRuntime(fullInfo?.runtime)
+        : fullInfo?.number_of_seasons === 1
+        ? `${fullInfo.seasons?.[0]?.name}개`
+        : `시즌 ${fullInfo?.number_of_seasons}개`,
+    overView: fullInfo?.overview,
     cast: fullInfo?.credits ? fullInfo?.credits.cast : [],
     recommend: fullInfo?.recommendations?.results ?? [],
   };
